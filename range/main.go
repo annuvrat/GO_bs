dc4031d0fbd4b22b6459e19ed7f64b3ce96a294f
annuvrat <annuvrat1098@gmail.com>
2025-12-28 16:54:33 +0530

    range in for loop

--- range/main.go
+++ range/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
  nums := []int{1,2,3,4,5}
-	for i:=0; i<len(nums); i++{
-		println(nums[i])
+	// for i:=0; i<len(nums); i++{
+	// 	println(nums[i])
+	// }
+    sum:=0
+	for _,num := range nums{
+		sum =sum+num
 	}
+	fmt.Println(sum)
 }
