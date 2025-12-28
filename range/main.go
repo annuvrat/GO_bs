ac3a060e6b8fec8689c41a43e1926870bfbdeb03
annuvrat <annuvrat1098@gmail.com>
2025-12-28 17:00:14 +0530

    iterate over string give value in unicode

--- range/main.go
+++ range/main.go
@@ -13,9 +13,14 @@ func main() {
 	// 	// fmt.Println(i)
 	// 	fmt.Println(num,i)
 	// }
-	k:= map[string]int{"price":100,"age":22}
+	// k:= map[string]int{"price":100,"age":22}
 
-	for key,value:= range k{
-		fmt.Println(key,value)
+	// for key,value:= range k{
+	// 	fmt.Println(key,value)
+	// }
+
+// value will be a unicode code point
+	for i,c:= range "hello"{
+		fmt.Println(i,c)
 	}
 }
