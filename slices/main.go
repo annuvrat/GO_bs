abd8455733f3d9419ca3f915a81e30b6c96e530c
annuvrat <annuvrat1098@gmail.com>
2025-12-28 16:23:59 +0530

    init commit

--- slices/main.go
+++ slices/main.go
@@ -1,20 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main(){
-// 	var nums [3]int
+	// var nums [3]int
 //   fmt.Println(nums)
 
 
 // var nums = make([]int,2,5)
 // //cap= capacity
+
+// nums = append(nums,1)
+// nums = append(nums,2)
+// nums = append(nums,3)
+// nums = append(nums,4)
+// nums = append(nums,5)
 // var nums2 = make([]int,len(nums))
 // // fmt.Println(cap(nums))
 // nums = append(nums,2)
 // fmt.Println(nums,nums2)
 // fmt.Println(cap(nums))
 var nums = []int{1,2,3,4,5}
-fmt.Println(nums[:1])
+var num2 = []int{1,2,3,4,5}
+fmt.Println(slices.Equal(nums,num2))
 
 }
