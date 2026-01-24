0f595d9acd3a19696f2869dd9d3cc427283a72b8
annuvrat <annuvrat1098@gmail.com>
2026-01-24 18:56:55 +0530

    feat : first citizeb funcs kimnda like closures

--- functions/main.go
+++ functions/main.go
@@ -2,24 +2,39 @@ package main
 
 import "fmt"
 
+// import "fmt"
 
-// func add(a, b int)int{
-// 	return a+b
+// // func add(a, b int)int{
+// // 	return a+b
+// // }
+
+// func main(){
+// 	// result:= add(3,4)
+// lang1,lang2,lang3:= lang()
+// fmt.Println(lang1)
+// fmt.Println(lang2)
+// fmt.Println(lang3)
+
+// 	// fmt.Println("The sum is:",result)
+// 	fmt.Println(lang())
 // }
 
+// func lang()(string,string,string){
+// 	return "GoLanguage","Golang","Go"
+// }
+
+
+func closure()func(a int)int{
+	return func(a int)int{
+		return a*a
+	}
+}
+
+
 func main(){
-	// result:= add(3,4)
+  
+	result:= closure()
+	fmt.Println(result(3))
 
-	
-
-	// fmt.Println("The sum is:",result)
-	fmt.Println(lang())
 }
-
-
-func lang()(string,string,string){
-	return "GoLanguage","Golang","Go"
-}
-
-
 
