b095fc2822d03ee930b7aacdae316f6c9d442559
annuvrat <annuvrat1098@gmail.com>
2026-01-24 18:32:35 +0530

    feat: functions with multiple returns

---
+++ functions/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+// func add(a, b int)int{
+// 	return a+b
+// }
+
+func main(){
+	// result:= add(3,4)
+
+	
+
+	// fmt.Println("The sum is:",result)
+	fmt.Println(lang())
+}
+
+
+func lang()(string,string,string){
+	return "GoLanguage","Golang","Go"
+}
+
+
+
