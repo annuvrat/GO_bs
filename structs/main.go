a26e13d41d76cc7512d7bb534b691e99907a9fb1
annuvrat <annuvrat1098@gmail.com>
2026-01-25 18:05:19 +0530

    inline structs

--- structs/main.go
+++ structs/main.go
@@ -2,55 +2,80 @@ package main
 
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
 
-type order struct{
-	id string
-	amount float32
-	status string
-	createdAt time.Time  //nano second precision
+// type order struct{
+// 	id string
+// 	amount float32
+// 	status string
+// 	createdAt time.Time  //nano second precision
+// }
+
+// func newOrder(id string, amount float32, status string)*order{
+// myOrder:= order{
+// 	id: id,
+// 	amount: amount,
+// 	status: status,
+// }
+// return &myOrder
+// }
+// func (o *order)changeStatus(status string){
+// 	o.status=status
+
+// }
+
+// func (o order)getAmount()float32{
+
+// 	return o.amount
+
+// }
+// func main() {
+
+// 	myorder:=order{
+// 		//if you  dont initialize fields, they get zero or default values and for string its ""
+// 		//int =0
+// 		//string=""
+// 		//float=0.0
+// 		//bool=false
+// 		id: "1234",
+// 		// amount: 250.5,
+// 		status: "created",
+// 		// createdAt: time.Now(),
+// 	}
+
+
+// 	MYorder2:= order{
+// 		id: "5678",
+// 		amount: 100.0,
+// 		// status: "shipped",
+// 		createdAt: time.Now(),
+// 	}
+//     myorder.changeStatus("confirmed")
+// 	// fmt.Println("Order after changeStatus call:", myorder)
+// 	myorder.createdAt = time.Now()
+// 	// MYorder2.status = "delivered"
+// 	fmt.Println("Order Amount:", myorder.getAmount())
+
+// // fmt.Println("Order before update:", myorder)
+// fmt.Println("Second Order:", MYorder2)
+// // fmt.Println("Order created at:", myorder.createdAt)
+// }
+
+
+
+func  main() {
+
+  language:=struct{
+	name string
+	year int
+  }{
+	name: "Go",
+	year: 2009,
+  }
+  fmt.Println("Language:", language)
 }
 
-func (o *order)changeStatus(status string){
-	o.status=status
-
-}
-
-func (o order)getAmount()float32{
-
-	return o.amount
-
-}
-func main() {
-
-	myorder:=order{
-		//if you  dont initialize fields, they get zero or default values and for string its ""
-		//int =0
-		//string=""
-		//float=0.0
-		//bool=false
-		id: "1234",
-		// amount: 250.5,
-		status: "created",
-		// createdAt: time.Now(),
-	}
 
 
-	MYorder2:= order{
-		id: "5678",
-		amount: 100.0,
-		// status: "shipped",
-		createdAt: time.Now(),
-	}
-    myorder.changeStatus("confirmed")
-	// fmt.Println("Order after changeStatus call:", myorder)
-	myorder.createdAt = time.Now()
-	// MYorder2.status = "delivered"
-	fmt.Println("Order Amount:", myorder.getAmount())
-
-// fmt.Println("Order before update:", myorder)
-fmt.Println("Second Order:", MYorder2)
-// fmt.Println("Order created at:", myorder.createdAt)
-}
