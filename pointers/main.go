3ddb74229e86cba52140d855cc0b05dcb903ac45
annuvrat <annuvrat1098@gmail.com>
2026-01-25 14:08:26 +0530

    now bu using pointer the main function num value is also changed

--- pointers/main.go
+++ pointers/main.go
@@ -4,17 +4,37 @@ import "fmt"
 
 
 
-func number(num int){
+// func number(num int){
+// // in this case num is a copy of main function num
+// // so changing num here will not affect main function num
+// 	num =5
+// 	fmt.Println("Inside number function:", num)
 
-	num =5
-	fmt.Println("Inside number function:", num)
+// }
+// func main(){
+// 	num:=1
+
+// 	number(num)
+
+// 	fmt.Println("Inside main function:", num)
+
+// }
+// lets now see how to use pointers to change the value of num in main function
+// by reference
+
+func changeNum(num*int){
+	// here num is a pointer to main function num
+	// so changing the value at the address will affect main function num
+	*num =5
+	fmt.Println("Inside changeNum function:", *num)
 
 }
 func main(){
-	num:=1
+num:=1
 
-	number(num)
+changeNum(&num)
 
-	fmt.Println("Inside main function:", num)
+fmt.Println("Inside main function:", num)
+// fmt.Println("memory address:", &num)
 
 }
