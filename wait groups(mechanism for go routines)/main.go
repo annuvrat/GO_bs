4dfb69e21c4bd14f082006542bb63f987d640e85
annuvrat <annuvrat1098@gmail.com>
2026-01-26 17:27:17 +0530

    wait groups use pointer wait , done and defer

--- wait groups(mechanism for go routines)/main.go
+++ wait groups(mechanism for go routines)/main.go
@@ -1,27 +1,31 @@
 package main
 
+import "sync"
+
 // goroutines are light wieght threads managed by go runtime
 import (
 	"fmt"
-	"time"
+	// "time"
 )
 
-func task(id int){
+func task(id int,w *sync.WaitGroup){
+
+	defer w.Done()
 	fmt.Println("Task",id,"is being processed")
 }
 
 func main(){
+	var wg sync.WaitGroup
   for i :=0;i<=10;i++{
 	// go task(i)
 
- go	func (i int){
-		task(i)
-	}(i)
+//  go	func (i int){
+	wg.Add(1)
+		go task(i,&wg)
+	// }(i)
   }
  
-
-  time.Sleep(time.Second*2)// wait group is used in real world scenario instead of sleep
-	// task(1)
+wg.Wait()
 
 }
 
