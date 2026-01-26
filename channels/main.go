7e90896049b449ebba38b9ffd77914540b9b543c
annuvrat <annuvrat1098@gmail.com>
2026-01-26 20:25:57 +0530

    buffered channel make (chan, 100)

--- channels/main.go
+++ channels/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 	// "math/rand"
 	// "time"
 )
+
 // sending example
 // func processNum(numChan chan int){
 // for num:=range numChan{
@@ -19,15 +21,13 @@ import (
 
 // }
 
-
 // func main(){
-
 
 // 	result:= make(chan int)
 // 	go sum(result,3,5)
 
 // 	res:=<-result
-	
+
 // 	fmt.Println("Sum is:",res)
 // 	numChan := make(chan int)
 
@@ -36,7 +36,7 @@ import (
 //    for {
 // 	numChan <- rand.Intn(100)
 //    }
-	// time.Sleep(time.Second*2)
+// time.Sleep(time.Second*2)
 // 	messageChan:= make(chan string)
 
 //   messageChan<-"ping"//blocking
@@ -47,21 +47,56 @@ import (
 
 // }
 
+//go routine synchronization using channels
+// func task(done chan bool){
+// defer func (){done<-true}()
+// fmt.Println("task started")
 
-func task(done chan bool){
-defer func (){done<-true}()
-fmt.Println("task started")
+// }
+
+// func main(){
+
+//  done:= make (chan bool)
+
+//  go task(done)
+
+//  <-done
+
+// }
+
+//buffered channels
+// make(chan int)      // Unbuffered: Send BLOCKS until receive
+// make(chan int, 100) // Buffered: Holds 100 values before blocking
+
+
+func emailSender(emailChan chan string,done chan bool){
+
+defer func(){done<-true}()
+
+for email:=range emailChan{
+	fmt.Println("sending email to ",email)
+	time.Sleep(time.Second)
+}
 
 
 }
 
-
 func main(){
+	emailChan:= make(chan string,100)// buffered channel have the space to hold 100 messages
+	
+	done:=make(chan bool)
 
- done:= make (chan bool)
+	go emailSender(emailChan,done)
 
- go task(done)
 
- <-done
+	for i:=0;i<5;i++{
 
+		emailChan<-fmt.Sprintf("%d@gmail.com",i)
+	
+	}
+	
+
+	fmt.Println("done sending")
+	close(emailChan)
+	<-done
 }
