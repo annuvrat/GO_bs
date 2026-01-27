cac338f84a6bc4ae83860564574ce829c1505f56
annuvrat <annuvrat1098@gmail.com>
2026-01-27 16:40:42 +0530

    mutex are used  to avoid race condition in concurrent processess

---
+++ mutex/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+
+
+type post struct{
+
+	views int
+}
+
+
+func (p * post ) inc(wg *sync.WaitGroup){
+	defer wg.Done()
+
+	p.views+= 1
+
+}
+func main(){
+   
+	var wg sync.WaitGroup
+	mypost:=post{views:0}
+
+
+	for i:=0;i<100;i++{
+		wg.Add(1)
+		go mypost.inc(&wg)
+	}
+
+	// mypost.inc()    
+    // mypost.inc()
+    wg.Wait()
+	fmt.Println(" my post ,",mypost.views)
+
+
+}
