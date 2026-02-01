5b6f07100d1409512468e869461d09d54749eb04
annuvrat <annuvrat1098@gmail.com>
2026-02-01 16:30:36 +0530

    packages and go mod init and how to export a function with first word as capital

---
+++ packages/auth/creds.go
@@ -0,0 +1,14 @@
+package auth
+
+import "fmt"
+
+
+func LoginWithCreds( username string, password string){
+
+
+fmt.Println("login user  using",username,password)
+
+}
+
+
+
---
+++ packages/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/annuvrat/GO_bs/auth"
+
+
+
+
+func main(){
+
+auth.LoginWithCreds("annu","mypassword")
+}
