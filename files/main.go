d9f275b700ec0910992627ee674a72d76bac6d05
annuvrat <annuvrat1098@gmail.com>
2026-02-01 15:44:35 +0530

    to read folders and files

--- files/main.go
+++ files/main.go
@@ -1,31 +1,77 @@
 package main
 
 import (
+	// "fmt"
 	"fmt"
 	"os"
 )
 
 func main(){
 
-f,err := os.Open("t.txt")
+// f,err := os.Open("t.txt")
 
-if err!=nil{
-	//log the error
+// if err!=nil{
+// 	//log the error
+// 	panic(err)
+// }
+
+//   fileInfo ,err:=f.Stat()
+
+//   if err!=nil{
+// 	panic(err)
+//   }
+
+//   fmt.Println("file name",fileInfo.Name())
+//   fmt.Println("file or folder",fileInfo.IsDir())
+//   fmt.Println("file size:",fileInfo.Size())
+//     fmt.Println("perms",fileInfo.Mode())
+// 	  fmt.Println("file mod at",fileInfo.ModTime())
+
+//read file
+// f,err:=  os.Open("t.txt")
+// if err!=nil{
+// 	panic(err)	
+
+
+// }
+
+// defer f.Close()
+
+// buf:= make([]byte,13)
+
+// d,err :=  f.Read(buf)
+
+// if err!= nil{
+// 	panic(err)
+// }
+
+// for i:=0;i<len(buf);i++{
+
+// println("data",d,string(buf[i]))
+
+// }
+
+// more direct approach not adivisable always only for small files
+// f,err := os.ReadFile("t.txt")
+
+// if err!=nil{
+// 	panic(err)
+// }
+// fmt.Println("data",string(f))
+
+//read folders
+dir,err:= os.Open("./")
+
+if err != nil{
 	panic(err)
 }
 
-  fileInfo ,err:=f.Stat()
+defer dir.Close()
 
-  if err!=nil{
-	panic(err)
-  }
+fileINfo,err:= dir.ReadDir(2)
 
-  fmt.Println("file name",fileInfo.Name())
-  fmt.Println("file or folder",fileInfo.IsDir())
-  fmt.Println("file size:",fileInfo.Size())
-    fmt.Println("perms",fileInfo.Mode())
-	  fmt.Println("file mod at",fileInfo.ModTime())
-
-
+for _,file:= range fileINfo {
+	fmt.Println("name",file.Name())
+}
 
 }
